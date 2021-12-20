873e486ff97edbed280aa0fc5411b8a93b7d7163
rahul20232 <89512251+rahul20232@users.noreply.github.com>
2021-12-20 14:02:09 +0530

    Add files via upload

--- demo/src/main/java/com/example/demo/HelloController.java
+++ demo/src/main/java/com/example/demo/HelloController.java
@@ -5,16 +5,23 @@ import javafx.animation.TranslateTransition;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.Toggle;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.paint.Paint;
+import javafx.scene.shape.Rectangle;
+import javafx.stage.Stage;
 import javafx.util.Duration;
 
 import java.io.File;
-import java.lang.reflect.Array;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.ResourceBundle;
@@ -36,15 +43,71 @@ public class HelloController implements Initializable {
     void findloc(MouseEvent event) {
         localval.setText("X:"+String.valueOf(event.getSceneX())+"Y:"+String.valueOf(event.getSceneY()));
     }
+    @FXML
+    private Button rollButton;
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
+    @FXML
+    private Rectangle red1;
+    @FXML
+    private Rectangle red2;
+    @FXML
+    private Rectangle black1;
+    @FXML
+    private Rectangle black2;
+    @FXML
+    private Label Player1;
+    @FXML
+    private Label Player2;
+    public void switchtoScene1(ActionEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("hello-view.fxml"));
+        Parent root = (Parent) loader.load();
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+    public void switchtoSceneoriginal(ActionEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("Scene1.fxml"));
+        Parent root = (Parent) loader.load();
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+    public void switchtoScene2(ActionEvent event) throws IOException{
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("Scene2.fxml"));
+        Parent root = (Parent) loader.load();
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+    public void switchtoScene3(ActionEvent event) throws IOException{
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("Scene3.fxml"));
+        Parent root = (Parent) loader.load();
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
     Player p1,p2;
     @FXML
     private Label diceVal;
     ArrayList<Tile> alltiles = new ArrayList<Tile>();
     ArrayList<SNL> Snakes_and_Ladders = new ArrayList<SNL>();
-    public void move(ActionEvent e) {
+    public void move(ActionEvent e) throws IOException {
         int times = (int)(Math.random()*6+1);
         String str = Integer.toString(times);
         diceVal.setText(str);
+        if(p1.getTurn()){
+            p2.black.setOpacity(1);
+        }
+        if(p2.getTurn()){
+            p1.black.setOpacity(1);
+        }
         if(times==1){
             File f = new File("/Users/sandeepsehgal/IdeaProjects/demo/src/main/resources/com/example/demo/1.png");
             Dice.setImage(new Image(f.toURI().toString()));
@@ -75,6 +138,26 @@ public class HelloController implements Initializable {
             roll(p1,times);
             p1.setTurn();
             p2.setTurn();
+            if(p1.getTurn()){
+                p1.black.setOpacity(0);
+                p1.red.setOpacity(0.67);
+                p1.P.setTextFill(Paint.valueOf("white"));
+            }
+            else{
+                p1.black.setOpacity(1);
+                p1.red.setOpacity(0);
+                p1.P.setTextFill(Paint.valueOf("black"));
+            }
+            if(p2.getTurn()){
+                p2.black.setOpacity(0);
+                p2.red.setOpacity(0.67);
+                p2.P.setTextFill(Paint.valueOf("white"));
+            }
+            else{
+                p2.black.setOpacity(1);
+                p2.red.setOpacity(0);
+                p2.P.setTextFill(Paint.valueOf("black"));
+            }
             return;
         }
 //        System.out.println("reached checkpoint 1");
@@ -83,6 +166,26 @@ public class HelloController implements Initializable {
             roll(p2,times);
             p2.setTurn();
             p1.setTurn();
+            if(p1.getTurn()){
+                p1.black.setOpacity(0);
+                p1.red.setOpacity(0.67);
+                p1.P.setTextFill(Paint.valueOf("white"));
+            }
+            else{
+                p1.black.setOpacity(1);
+                p1.red.setOpacity(0);
+                p1.P.setTextFill(Paint.valueOf("black"));
+            }
+            if(p2.getTurn()){
+                p2.black.setOpacity(0);
+                p2.red.setOpacity(0.67);
+                p2.P.setTextFill(Paint.valueOf("white"));
+            }
+            else{
+                p2.black.setOpacity(1);
+                p2.red.setOpacity(0);
+                p2.P.setTextFill(Paint.valueOf("black"));
+            }
             return;
         }
         if(p1.getTurn() && p1.getHasStarted()){
@@ -96,8 +199,34 @@ public class HelloController implements Initializable {
         }
         p1.setTurn();
         p2.setTurn();
+        if(p1.getTurn()){
+            p1.black.setOpacity(0);
+            p1.red.setOpacity(0.67);
+            p1.P.setTextFill(Paint.valueOf("white"));
+        }
+        else{
+            p1.black.setOpacity(1);
+            p1.red.setOpacity(0);
+            p1.P.setTextFill(Paint.valueOf("black"));
+        }
+        if(p2.getTurn()){
+            p2.black.setOpacity(0);
+            p2.red.setOpacity(0.67);
+            p2.P.setTextFill(Paint.valueOf("white"));
+        }
+        else{
+            p2.black.setOpacity(1);
+            p2.red.setOpacity(0);
+            p2.P.setTextFill(Paint.valueOf("black"));
+        }
+        if(p1.current.getId()==100){
+            switchtoScene2(e);
+        }
+        if(p2.current.getId()==100){
+            switchtoScene3(e);
+        }
     }
-    public void roll(Player p, int r){
+    public void roll(Player p, int r) throws IOException {
         if(p.current.getId()+r>100){
             return;
         }
@@ -184,6 +313,10 @@ public class HelloController implements Initializable {
             movement.play();
         System.out.println(r);
         p.setprevMove(r);
+        if(p.current.getId()==100){
+            ActionEvent event = new ActionEvent();
+            switchtoScene2(event);
+        }
     }
     Image dice1,dice2,dice3,dice4,dice5,dice6;
     @Override
@@ -212,8 +345,8 @@ public class HelloController implements Initializable {
 
         Tile t = new Tile(0.0,60.0,0);
         t.setNextTile(alltiles.get(0));
-        p1 = new Player(goti1, true,t);
-        p2 = new Player(goti2, false,t);
+        p1 = new Player(goti1, true,t,red1, black1, Player1);
+        p2 = new Player(goti2, false,t,red2, black2, Player2);
         ArrayList<Tile> A1 = new ArrayList<Tile>();
         A1.add(alltiles.get(27));A1.add(alltiles.get(26));A1.add(alltiles.get(13));A1.add(alltiles.get(12));A1.add(alltiles.get(13));
         ArrayList<Tile> A2 = new ArrayList<Tile>();
@@ -237,37 +370,23 @@ public class HelloController implements Initializable {
         Snakes_and_Ladders.add(new Ladder(alltiles.get(4),alltiles.get(16)));
         Snakes_and_Ladders.add(new Ladder(alltiles.get(6),alltiles.get(14)));
         Snakes_and_Ladders.add(new Ladder(alltiles.get(8),alltiles.get(12)));
-        Snakes_and_Ladders.add(new Snakes(alltiles.get(27),alltiles.get(13),A1,true,true));
-        Snakes_and_Ladders.add(new Snakes(alltiles.get(25),alltiles.get(15),A2,true,true));
-        Snakes_and_Ladders.add(new Snakes(alltiles.get(23),alltiles.get(17),A3,true,true));
+//        Snakes_and_Ladders.add(new Snakes(alltiles.get(27),alltiles.get(13),A1,true,true));
+//        Snakes_and_Ladders.add(new Snakes(alltiles.get(25),alltiles.get(15),A2,true,true));
+//        Snakes_and_Ladders.add(new Snakes(alltiles.get(23),alltiles.get(17),A3,true,true));
         Snakes_and_Ladders.add(new Ladder(alltiles.get(32),alltiles.get(46)));
         Snakes_and_Ladders.add(new Ladder(alltiles.get(34),alltiles.get(44)));
         Snakes_and_Ladders.add(new Ladder(alltiles.get(36),alltiles.get(42)));
         Snakes_and_Ladders.add(new Ladder(alltiles.get(39),alltiles.get(80)));
-        Snakes_and_Ladders.add(new Snakes(alltiles.get(54),alltiles.get(33),A4,true,false));
-        Snakes_and_Ladders.add(new Snakes(alltiles.get(56),alltiles.get(35),A5,true,false));
-        Snakes_and_Ladders.add(new Snakes(alltiles.get(58),alltiles.get(37),A6,true,false));
+//        Snakes_and_Ladders.add(new Snakes(alltiles.get(54),alltiles.get(33),A4,true,false));
+//        Snakes_and_Ladders.add(new Snakes(alltiles.get(56),alltiles.get(35),A5,true,false));
+//        Snakes_and_Ladders.add(new Snakes(alltiles.get(58),alltiles.get(37),A6,true,false));
         Snakes_and_Ladders.add(new Ladder(alltiles.get(67),alltiles.get(86)));
         Snakes_and_Ladders.add(new Ladder(alltiles.get(65),alltiles.get(84)));
         Snakes_and_Ladders.add(new Ladder(alltiles.get(63),alltiles.get(82)));
-        Snakes_and_Ladders.add(new Snakes(alltiles.get(90),alltiles.get(49),A10,false,false));
-        Snakes_and_Ladders.add(new Snakes(alltiles.get(94),alltiles.get(73),A7,true,false));
-        Snakes_and_Ladders.add(new Snakes(alltiles.get(96),alltiles.get(75),A8,true,false));
-        Snakes_and_Ladders.add(new Snakes(alltiles.get(98),alltiles.get(77),A9,true,false));
-        int count = 1000;
-        while(count-->0){
-            SequentialTransition arrowTransition = new SequentialTransition(arrow);
-            TranslateTransition trans = new TranslateTransition();
-            trans.setDuration(Duration.millis(20));
-            trans.setByY(100);
-            arrowTransition.getChildren().add(trans);
-            TranslateTransition trans1 = new TranslateTransition();
-            trans1.setDuration(Duration.millis(20));
-            trans1.setByY(-100);
-            arrowTransition.getChildren().add(trans1);
-            arrowTransition.play();
-        }
-
+//        Snakes_and_Ladders.add(new Snakes(alltiles.get(90),alltiles.get(49),A10,false,false));
+//        Snakes_and_Ladders.add(new Snakes(alltiles.get(94),alltiles.get(73),A7,true,false));
+//        //Snakes_and_Ladders.add(new Snakes(alltiles.get(96),alltiles.get(75),A8,true,false));
+        //Snakes_and_Ladders.add(new Snakes(alltiles.get(98),alltiles.get(77),A9,true,false));
     }
     class Tile{
         private double x,y;
@@ -296,6 +415,9 @@ public class HelloController implements Initializable {
     class Player{
         Tile current;
         ImageView img;
+        Rectangle red;
+        Rectangle black;
+        Label P;
         private boolean start = true;
         private boolean rightup = false;
         private boolean leftup = false;
@@ -303,10 +425,13 @@ public class HelloController implements Initializable {
         private int prevMove;
         private boolean turn;
         private boolean hasStarted = false;
-        public Player(ImageView img1, boolean f,Tile current){
+        public Player(ImageView img1, boolean f,Tile current, Rectangle red, Rectangle black, Label P){
             this.img = img1;
             this.turn = f;
             this.current = current;
+            this.red = red;
+            this.black = black;
+            this.P = P;
 //            System.out.println("Player completed with turn: "+ f);
 //            System.out.println(this.img==null);
         }
